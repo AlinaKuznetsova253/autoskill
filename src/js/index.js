72bed8e6f5730f49549714204d1c69af6f6421a9
Alina <kuznetsova646@gmail.com>
2025-08-27 05:13:04 +0500

    added a modal window for sending a number and its adaptivity

--- src/js/index.js
+++ src/js/index.js
@@ -54,4 +54,68 @@ document.addEventListener("DOMContentLoaded", () => {
 
     map.geoObjects.add(placemark);
   }
+
+  const modalController = ({ modal, btnOpen, btnClose, time = 300 }) => {
+    const buttonElems = document.querySelectorAll(btnOpen);
+    const modalElem = document.querySelector(modal);
+
+    modalElem.style.cssText = `
+   display: flex;
+   visibility: hidden;
+   opacity: 0;
+   transition: opacity ${time}ms ease-in-out;
+   `;
+
+    const closeModal = (event) => {
+      const target = event.target;
+
+      if (
+        target === modalElem ||
+        (btnClose && target.closest(btnClose)) ||
+        event.code === "Escape"
+      ) {
+        modalElem.style.opacity = 0;
+
+        setTimeout(() => {
+          modalElem.style.visibility = "hidden";
+        }, time);
+
+        window.removeEventListener("keydown", closeModal);
+      }
+    };
+
+    const openModal = () => {
+      modalElem.style.visibility = "visible";
+      modalElem.style.opacity = 1;
+      window.addEventListener("keydown", closeModal);
+    };
+
+    buttonElems.forEach((btn) => {
+      btn.addEventListener("click", openModal);
+    });
+    modalElem.addEventListener("click", closeModal);
+  };
+
+  modalController({
+    modal: ".modal",
+    btnOpen: ".hero__btn",
+    btnClose: ".modal__close",
+  });
+
+  const consentCheckbox = document.getElementById("consent");
+  const submitBtn = document.getElementById("submitBtn");
+  const form = document.querySelector(".modal-form");
+
+  consentCheckbox.addEventListener("change", function () {
+    submitBtn.disabled = !consentCheckbox.checked;
+  });
+
+  form.addEventListener("submit", function (e) {
+    if (!consentCheckbox.checked) {
+      e.preventDefault();
+      alert(
+        "Пожалуйста, подтвердите согласие на обработку персональных данных.",
+      );
+    }
+  });
 });
